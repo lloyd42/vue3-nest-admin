52988c65ec1b25157a6487e0f97b139fb254a589
lloyd42 <49894164+lloyd42@users.noreply.github.com>
2022-01-14 16:31:04 +0800

    添加mysql、typeorm

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +7,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiTags('getHello')
+  @ApiOperation({
+    summary: '测试',
+  })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = '3000';
+  console.log(`Listen in http://localhost:${port}`);
+
+  const options = new DocumentBuilder()
+    .setTitle('Vue3 Nest Admin')
+    .setDescription('为 Vue3 Admin 提供 API')
+    .setVersion('1.0')
+    .addTag('Vue3 Admin')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
